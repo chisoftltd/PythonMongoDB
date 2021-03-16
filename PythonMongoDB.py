a3348b292383bcccf7334eea564b0c90a35192c1
chisoftltd <chisoftltd@gmail.com>
2021-03-16 01:13:37 +0100

    first commit

---
+++ PythonMongoDB.py
@@ -0,0 +1,79 @@
+# Python MongoDB
+
+# import datetime module
+import datetime
+# import pymongo module
+import pymongo
+myclient = pymongo.MongoClient("mongodb+srv://Chisoft:9thMileCorner@cluster0.tnc8g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+mydb = myclient["ChisoftDB"]
+print(myclient.list_database_names())
+print()
+dblist = myclient.list_database_names()
+if "ChisoftDB" in dblist:
+  print("The database exists.")
+
+# Python MongoDB Insert Document
+mycol = mydb["customers"]
+
+mydict = { "name": "John", "address": "Highway 37" }
+
+x = mycol.insert_one(mydict)
+
+print(x)
+
+# sample data
+document = {"company":"Capital One",
+"city":"McLean",
+"state":"VA",
+"country":"US"}
+# insert document into collection
+id = mycol.insert_one(document).inserted_id
+print("id")
+print(id)
+
+print(x.inserted_id)
+
+# Insert Multiple Documents
+mylist = [
+  { "name": "Amy", "address": "Apple st 652"},
+  { "name": "Hannah", "address": "Mountain 21"},
+  { "name": "Michael", "address": "Valley 345"},
+  { "name": "Sandy", "address": "Ocean blvd 2"},
+  { "name": "Betty", "address": "Green Grass 1"},
+  { "name": "Richard", "address": "Sky st 331"},
+  { "name": "Susan", "address": "One way 98"},
+  { "name": "Vicky", "address": "Yellow Garden 2"},
+  { "name": "Ben", "address": "Park Lane 38"},
+  { "name": "William", "address": "Central st 954"},
+  { "name": "Chuck", "address": "Main Road 989"},
+  { "name": "Viola", "address": "Sideway 1633"}
+]
+
+x = mycol.insert_many(mylist)
+
+print(x.inserted_ids)
+
+
+# Insert Multiple Documents, with Specified IDs
+myEmp = mydb["Employees"]
+
+mylist2 = [
+  { "_id": 1, "name": "John", "address": "Highway 37"},
+  { "_id": 2, "name": "Peter", "address": "Lowstreet 27"},
+  { "_id": 3, "name": "Amy", "address": "Apple st 652"},
+  { "_id": 4, "name": "Hannah", "address": "Mountain 21"},
+  { "_id": 5, "name": "Michael", "address": "Valley 345"},
+  { "_id": 6, "name": "Sandy", "address": "Ocean blvd 2"},
+  { "_id": 7, "name": "Betty", "address": "Green Grass 1"},
+  { "_id": 8, "name": "Richard", "address": "Sky st 331"},
+  { "_id": 9, "name": "Susan", "address": "One way 98"},
+  { "_id": 10, "name": "Vicky", "address": "Yellow Garden 2"},
+  { "_id": 11, "name": "Ben", "address": "Park Lane 38"},
+  { "_id": 12, "name": "William", "address": "Central st 954"},
+  { "_id": 13, "name": "Chuck", "address": "Main Road 989"},
+  { "_id": 14, "name": "Viola", "address": "Sideway 1633"}
+]
+
+y = myEmp.insert_many(mylist2)
+print(x.inserted_ids)
